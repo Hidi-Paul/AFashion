4549607d4c3dbf49cd5990fd1420d9ad001bec57
Hidi Paul <paul.hidi@endava.com>
2017-08-29 16:44:58 +0300

    Removed Comments

--- AFashion/OCS.DataAccess/Migrations/Configuration.cs
+++ AFashion/OCS.DataAccess/Migrations/Configuration.cs
@@ -14,19 +14,6 @@ namespace OCS.DataAccess.Migrations
 
         protected override void Seed(Context.FashionContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-
             IList<Category> defaultCategs = new List<Category>();
             IList<Brand> defaultBrands = new List<Brand>();
             IList<Product> defaultProducts = new List<Product>();
