a6045ba3b0bccfc410066572034728ca692a23fa
Hidi Paul <paul.hidi@endava.com>
2017-09-01 12:49:19 +0300

    Added Unit Tests for Filters

---
+++ AFashion/OCS.UnitTests/BusinessLayer/Filters/CategoryFilterTests.cs
@@ -0,0 +1,114 @@
+﻿using Moq;
+using NUnit.Framework;
+using OCS.BusinessLayer.Filters;
+using OCS.DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCS.UnitTests.BusinessLayer.Filters
+{
+    [TestFixture]
+    class CategoryFilterTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            //Initializations
+
+        }
+
+        [Test]
+        public void Resolve_FiltersCorrectly()
+        {
+            //Arrange
+            var items = GetProductList();
+            var filterCategory = items[5].Category;
+            var filter = new CategoryFilter(items, filterCategory.Name);
+
+            var goodItems = new List<Product>();
+            for (int i = 0; i < items.Count(); i++)
+            {
+                if (items[i].Category == filterCategory)
+                {
+                    goodItems.Add(items[i]);
+                }
+            }
+
+            //Act
+            var result = filter.Resolve();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Filters);
+            Assert.IsTrue(result.Filters.ContainsKey("Category"));
+            var filteredItems = result.Filters["Category"] as IList<Product>;
+            Assert.IsTrue(goodItems.All(filteredItems.Contains));
+            Assert.IsTrue(filteredItems.All(goodItems.Contains));
+        }
+
+        [Test]
+        public void Resolve_StacksWithOtherFilters()
+        {
+            //Arrange
+
+            //Mock another Filter
+            var secondFilterItems = new List<Product>();
+            var secondFilterResults = new FilterResult();
+            secondFilterResults.AddFilter("secondFilter", secondFilterItems);
+
+            var secondFilter = new Mock<AbstractFilter>();
+            secondFilter.Setup(x => x.Resolve()).Returns(secondFilterResults);
+
+            var filter = new CategoryFilter(new List<Product>(), "SomeFilter", secondFilter.Object);
+
+            //Act
+            var result = filter.Resolve();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Filters);
+            Assert.IsTrue(result.Filters.ContainsKey("Category"));
+            Assert.IsTrue(result.Filters.ContainsKey("secondFilter"));
+        }
+
+        #region Helpers
+        private IList<Product> GetProductList()
+        {
+            var brands = new List<Brand>()
+            {
+                new Brand{ID=Guid.NewGuid(), Name="SampleBrand1"},
+                new Brand{ID=Guid.NewGuid(), Name="SampleBrand2"},
+                new Brand{ID=Guid.NewGuid(), Name="SampleBrand3"}
+            };
+
+            var categs = new List<Category>()
+            {
+                new Category{ID=Guid.NewGuid(), Name="SampleCategory1"},
+                new Category{ID=Guid.NewGuid(), Name="SampleCategory2"},
+                new Category{ID=Guid.NewGuid(), Name="SampleCategory3"}
+            };
+
+            var items = new List<Product>()
+            {
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct00", Image="http://someImage.jpg", Price=10, Brand=brands[0], Category=categs[0]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct01", Image="http://someImage.jpg", Price=11, Brand=brands[0], Category=categs[1]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct02", Image="http://someImage.jpg", Price=12, Brand=brands[0], Category=categs[2]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct03", Image="http://someImage.jpg", Price=13, Brand=brands[0], Category=categs[0]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct04", Image="http://someImage.jpg", Price=14, Brand=brands[1], Category=categs[1]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct05", Image="http://someImage.jpg", Price=15, Brand=brands[1], Category=categs[2]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct06", Image="http://someImage.jpg", Price=16, Brand=brands[1], Category=categs[0]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct07", Image="http://someImage.jpg", Price=17, Brand=brands[1], Category=categs[1]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct08", Image="http://someImage.jpg", Price=18, Brand=brands[1], Category=categs[2]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct09", Image="http://someImage.jpg", Price=19, Brand=brands[2], Category=categs[0]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct10", Image="http://someImage.jpg", Price=20, Brand=brands[2], Category=categs[1]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct11", Image="http://someImage.jpg", Price=21, Brand=brands[2], Category=categs[2]},
+                new Product{ID=Guid.NewGuid(), Name="SampleProduct12", Image="http://someImage.jpg", Price=22, Brand=brands[2], Category=categs[0]},
+            };
+            return items;
+        }
+        #endregion Helpers
+    }
+}
---
+++ AFashion/OCS.UnitTests/BusinessLayer/Filters/FilterResultTests.cs
@@ -0,0 +1,134 @@
+﻿using NUnit.Framework;
+using OCS.BusinessLayer.Filters;
+using OCS.DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCS.UnitTests.BusinessLayer.Filters
+{
+    [TestFixture]
+    public class FilterResultTests
+    {
+        private FilterResult filterResult;
+
+        [SetUp]
+        public void Init()
+        {
+            //Initializations
+            filterResult = new FilterResult();
+        }
+
+        [Test]
+        public void AddFilter_AddsFilterToDictionary()
+        {
+            //Arrange
+            string key1 = "key1";
+            string key2 = "key2";
+            string key3 = "key3";
+            var filter1 = new List<Product>();
+            var filter2 = new List<Product>();
+            var filter3 = new List<Product>();
+
+            //Act
+            filterResult.AddFilter(key1, filter1);
+            filterResult.AddFilter(key2, filter2);
+            filterResult.AddFilter(key3, filter3);
+
+            //Assert
+            Assert.NotNull(filterResult.Filters);
+            Assert.IsNotEmpty(filterResult.Filters);
+            Assert.IsTrue(filterResult.Filters.ContainsKey(key1));
+            Assert.IsTrue(filterResult.Filters[key1] == filter1);
+            Assert.IsTrue(filterResult.Filters.ContainsKey(key2));
+            Assert.IsTrue(filterResult.Filters[key2] == filter2);
+            Assert.IsTrue(filterResult.Filters.ContainsKey(key3));
+            Assert.IsTrue(filterResult.Filters[key3] == filter3);
+        }
+
+        [Test]
+        public void AddFilter_SameFilterResultsDoNotOverwrite()
+        {
+            //Arrange
+            string key1 = "key1";
+            Product prod1 = new Product();
+            Product prod2 = new Product();
+            var filter1 = new List<Product>() { prod1 };
+            var filter2 = new List<Product>() { prod2 };
+
+            //Act
+            filterResult.AddFilter(key1, filter1);
+            filterResult.AddFilter(key1, filter2);
+
+            //Assert
+            Assert.IsTrue(filterResult.Filters.Count == 1);
+            Assert.IsTrue(filterResult.Filters.ContainsKey(key1));
+            Assert.IsTrue(filterResult.Filters[key1].Count() == 2);
+            Assert.IsTrue(filterResult.Filters[key1].Contains(prod1));
+            Assert.IsTrue(filterResult.Filters[key1].Contains(prod2));
+        }
+
+        [Test]
+        public void Result_ReturnsEmptyListIfNoFilters()
+        {
+            //Arrange
+
+            //Act
+            var result = filterResult.Result();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void Result_ReturnsFilteredProducts()
+        {
+            //Arrange
+            var items = new List<Product>()
+            {
+                new Product(),
+                new Product()
+            };
+
+            //Act
+            filterResult.AddFilter("someKey", items);
+            var result = filterResult.Result();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count() == 2);
+            Assert.IsTrue(items.All(result.Contains));
+            Assert.IsTrue(result.All(items.Contains));
+        }
+
+        [Test]
+        public void Result_ReturnsValidResultsWithStackingFilters()
+        {
+            //Arrange
+            var commonProduct = new Product();
+            var items1 = new List<Product>()
+            {
+                new Product(),
+                commonProduct
+            };
+            var items2 = new List<Product>()
+            {
+                commonProduct,
+                new Product()
+            };
+
+            //Act
+            filterResult.AddFilter("someKey1", items1);
+            filterResult.AddFilter("someKey2", items2);
+            var result = filterResult.Result();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count() == 1);
+            Assert.IsTrue(result.Contains(commonProduct));
+        }
+    }
+}
